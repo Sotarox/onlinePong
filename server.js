a8c2f9ae28d8de8ccd90a7420a7e13df008820e0
Sotarox <bluceislee@gmail.com>
2022-07-02 22:15:05 +0200

    Replace for => forEach

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ var Room = require("./server/room.js");
 var room01 = new Room('room01');
 var room02 = new Room('room02');
 var rooms = [room01, room02];
-setInterval(function () { room01.calculate() }, 10);
-setInterval(function () { room02.calculate() }, 10);
+setInterval(() => { room01.calculate() }, 10);
+setInterval(() => { room02.calculate() }, 10);
 
 function gameStateWatcher(room) {
   //watch scores
@@ -52,17 +52,17 @@ function gameStateWatcher(room) {
     room.gameOverSwitch = false;
   }
 }
-setInterval(function () { gameStateWatcher(room01) }, 10);
-setInterval(function () { gameStateWatcher(room02) }, 10);
+setInterval( () => { gameStateWatcher(room01) }, 10);
+setInterval( () => { gameStateWatcher(room02) }, 10);
 
 //sockets start
-io.on('connection', socket => {
+io.on('connection', (socket) => {
   console.log(socket.id + ' connected to server with socket ver: ', socket.conn.protocol);
   var chosenRoom = '';
   var clientName = '';
   var systemMessage = '';
 
-  socket.on('client_to_server_join', data => {
+  socket.on('client_to_server_join', (data) => {
     chosenRoom = data.room;
     socket.join(chosenRoom);
     clientName = data.clientName;
@@ -86,130 +86,129 @@ io.on('connection', socket => {
     io.to(chosenRoom).emit("showSystemMessage", { value: systemMessage });
   });
   //Start Button
-  socket.on("getStart", function (data) {
+  socket.on("getStart", (data) => {
     console.log(data.value + " Start Button is pressed");
-    for (var i = 0; i < rooms.length; i++) {
-      if (data.value === rooms[i].name) {
-        rooms[i].calcSwitch = true;
-        io.to(rooms[i].name).emit("setStart", { scoreBlue: rooms[i].scoreBlue, scoreRed: rooms[i].scoreRed });
+    rooms.forEach((room) => {
+      if (data.value === room.name) {
+        room.calcSwitch = true;
+        io.to(room.name).emit("setStart", { scoreBlue: room.scoreBlue, scoreRed: room.scoreRed });
       }
-    }
+    });
   });
   //to initialize ball&paddle coordinates
-  socket.on("getCoordinates", function (data) {
-    for (var i = 0; i < rooms.length; i++) {
-      if (data.value === rooms[i].name) {
-        io.to(rooms[i].name).emit("setCoordinates",
+  socket.on("getCoordinates", (data) => {
+    rooms.forEach((room) => {
+      if (data.value === room.name) {
+        io.to(room.name).emit("setCoordinates",
           {
-            ballX: rooms[i].ballX, 
-            ballY: rooms[i].ballY,
-            paddleX: rooms[i].paddleX, 
-            paddleX2: rooms[i].paddleX2,
-            paddleX3: rooms[i].paddleX3, 
-            paddleX4: rooms[i].paddleX4
+            ballX: room.ballX, 
+            ballY: room.ballY,
+            paddleX: room.paddleX, 
+            paddleX2: room.paddleX2,
+            paddleX3: room.paddleX3, 
+            paddleX4: room.paddleX4
           });
       }
-    }
+    });
   });
 
   //Control paddles
-  socket.on("rightKeyDown", function (data) {
+  socket.on("rightKeyDown", (data) => {
     console.log(data.room + ' ' + data.value + " :rightKey is pressed");
-    for (var i = 0; i < rooms.length; i++) {
-      if (data.room === rooms[i].name) {
+    rooms.forEach((room) => {
+      if (data.room === room.name) {
         if (data.value === 'Player1') {
-          rooms[i].rightPressed1 = true;
+          room.rightPressed1 = true;
         } else if (data.value === 'Player2') {
-          rooms[i].rightPressed2 = true;
+          room.rightPressed2 = true;
         } else if (data.value === 'Player3') {
-          rooms[i].rightPressed3 = true;
+          room.rightPressed3 = true;
         } else if (data.value === 'Player4') {
-          rooms[i].rightPressed4 = true;
+          room.rightPressed4 = true;
         }
       }
-    }
+    });
   });
-  socket.on("leftKeyDown", function (data) {
+  socket.on("leftKeyDown", (data) => {
     console.log(data.room + ' ' + data.value + " :leftKey is pressed");
-    for (var i = 0; i < rooms.length; i++) {
-      if (data.room === rooms[i].name) {
+    rooms.forEach ((room)=> {
+      if (data.room === room.name) {
         if (data.value === 'Player1') {
-          rooms[i].leftPressed1 = true;
+          room.leftPressed1 = true;
         } else if (data.value === 'Player2') {
-          rooms[i].leftPressed2 = true;
+          room.leftPressed2 = true;
         } else if (data.value === 'Player3') {
-          rooms[i].leftPressed3 = true;
+          room.leftPressed3 = true;
         } else if (data.value === 'Player4') {
-          rooms[i].leftPressed4 = true;
+          room.leftPressed4 = true;
         }
       }
-    }
+    });
   });
-  socket.on("rightKeyUp", function (data) {
+  socket.on("rightKeyUp", (data) => {
     console.log(data.room + ' ' + data.value + " :rightKey is released");
-    for (var i = 0; i < rooms.length; i++) {
-      if (data.room === rooms[i].name) {
+    rooms.forEach ((room)=> {
+      if (data.room === room.name) {
         if (data.value === 'Player1') {
-          rooms[i].rightPressed1 = false;
+          room.rightPressed1 = false;
         } else if (data.value === 'Player2') {
-          rooms[i].rightPressed2 = false;
+          room.rightPressed2 = false;
         } else if (data.value === 'Player3') {
-          rooms[i].rightPressed3 = false;
+          room.rightPressed3 = false;
         } else if (data.value === 'Player4') {
-          rooms[i].rightPressed4 = false;
+          room.rightPressed4 = false;
         }
       }
-    }
+    });
   });
-  socket.on("leftKeyUp", function (data) {
+  socket.on("leftKeyUp", (data) => {
     console.log(data.room + ' ' + data.value + " :leftKey is released");
-    for (var i = 0; i < rooms.length; i++) {
-      if (data.room === rooms[i].name) {
+    rooms.forEach ((room)=> {
+      if (data.room === room.name) {
         if (data.value === 'Player1') {
-          rooms[i].leftPressed1 = false;
+          room.leftPressed1 = false;
         } else if (data.value === 'Player2') {
-          rooms[i].leftPressed2 = false;
+          room.leftPressed2 = false;
         } else if (data.value === 'Player3') {
-          rooms[i].leftPressed3 = false;
+          room.leftPressed3 = false;
         } else if (data.value === 'Player4') {
-          rooms[i].leftPressed4 = false;
+          room.leftPressed4 = false;
         }
       }
-    }
+    });
   });
   //Pause Button
-  socket.on("getPause", function () {
+  socket.on("getPause", () => {
     console.log(chosenRoom + " Pause Button is pressed");
-    for (var i = 0; i < rooms.length; i++) {
-      if (chosenRoom === rooms[i].name) {
-        rooms[i].calcSwitch = false;
+    rooms.forEach ((room)=> {
+      if (chosenRoom === room.name) {
+        room.calcSwitch = false;
         io.to(chosenRoom).emit("setPause");
         systemMessage = clientName + " pressed Pause Button"
         io.to(chosenRoom).emit("showSystemMessage", { value: systemMessage });
       }
-    }
+    });
   });
   //Reset Button
-  socket.on("getReset", function (data) {
+  socket.on("getReset", (data) => {
     console.log(data.value + " Reset Button is pressed");
-    for (var i = 0; i < rooms.length; i++) {
-      if (data.value === rooms[i].name) {
-        rooms[i].scoreBlue = 0; rooms[i].scoreRed = 0;
+    rooms.forEach ((room)=> {
+      if (data.value === room.name) {
+        room.scoreBlue = 0; room.scoreRed = 0;
         io.to(data.value).emit("renewScore", { scoreBlue: 0, scoreRed: 0 });
-        rooms[i].ballX = 240; rooms[i].ballY = 270; rooms[i].balldy = -2;
-        rooms[i].gameOverSwitch = false;
-        rooms[i].calcSwitch = true;
+        room.ballX = 240; room.ballY = 270; room.balldy = -2;
+        room.gameOverSwitch = false;
+        room.calcSwitch = true;
       }
-    }
-
+    });
   });
   //chat
-  socket.on("client_to_server_chat", function (data) {
+  socket.on("client_to_server_chat", (data) => {
     systemMessage = data.value;
     io.to(chosenRoom).emit("showSystemMessage", { value: systemMessage });
   });
   //disconnect
-  socket.on('disconnect', function () {
+  socket.on('disconnect', () => {
     systemMessage = socket.id + ' ' + ' ' + chosenRoom + ' ' + clientName + ' has disconnected';
     console.log(systemMessage);
     for (var i = 0; i < players.length; i++) {
